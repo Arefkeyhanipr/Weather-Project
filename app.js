f17fecda693c4f8c024752d1140f2dbd294bba69
Aref <Arefkeyhani.pr@gmail.com>
2025-02-12 02:52:53 +0330

    setting up the site that we want the data from and api

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const searchInput = document.querySelector(".search-input");
 const searchBtn = document.querySelector(".search-Btn");
 
+const apiKey = "dec9e32983e1e4b158d7e7b071f4bc9d";
+
 searchBtn.addEventListener("click", () => {
   if (searchInput.value.trim() != "") {
     searchInput.blur();
+
+    updateWeatherInfo(searchInput.value);
 
     searchInput.value = "";
   }
@@ -13,6 +17,22 @@ searchInput.addEventListener("keydown", (e) => {
   if (e.key == "Enter" && searchInput.value.trim() != "") {
     searchInput.blur();
 
+    updateWeatherInfo(searchInput.value);
+
     searchInput.value = "";
   }
 });
+
+async function getFetchData(endpoint, city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/${endpoint}?q=${city}&appid=${apiKey}&units=metric`;
+
+  const response = await fetch(apiUrl);
+
+  return response.json();
+}
+
+async function updateWeatherInfo(city) {
+  const weatherData = await getFetchData("weather", city);
+
+  console.log(weatherData);
+}
