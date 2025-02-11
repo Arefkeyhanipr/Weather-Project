e224c0eb1e656588da0203a1717179e646cacf0a
Aref <Arefkeyhani.pr@gmail.com>
2025-02-12 03:07:40 +0330

    setting up the display for not found,search city and the weather info of the cities

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const searchInput = document.querySelector(".search-input");
 const searchBtn = document.querySelector(".search-Btn");
 
+const WeatherInfoSection = document.querySelector(".weather-info");
+const notFoundSection = document.querySelector(".not-found");
+const searchCitySection = document.querySelector(".search-city");
+
 const apiKey = "dec9e32983e1e4b158d7e7b071f4bc9d";
 
 searchBtn.addEventListener("click", () => {
@@ -34,5 +38,18 @@ async function getFetchData(endpoint, city) {
 async function updateWeatherInfo(city) {
   const weatherData = await getFetchData("weather", city);
 
-  console.log(weatherData);
+  if (weatherData.cod != 200) {
+    showDisplaySection(notFoundSection);
+    return;
+  }
+
+  showDisplaySection(WeatherInfoSection);
+}
+
+function showDisplaySection(section) {
+  [WeatherInfoSection, searchCitySection, notFoundSection].forEach(
+    (section) => (section.style.display = "none")
+  );
+
+  section.style.display = "flex";
 }
