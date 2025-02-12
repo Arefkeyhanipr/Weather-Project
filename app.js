5b3940419a6980fcecaaeaf553e96f129bc479c2
Aref <Arefkeyhani.pr@gmail.com>
2025-02-12 04:27:45 +0330

    matching the values of weather info with api data

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const WeatherInfoSection = document.querySelector(".weather-info");
 const notFoundSection = document.querySelector(".not-found");
 const searchCitySection = document.querySelector(".search-city");
 
+const countryText = document.querySelector(".country-text");
+const tempText = document.querySelector(".temp-text");
+const conditionText = document.querySelector(".condition-text");
+const humidityValueText = document.querySelector(".humidity-value-text");
+const windValueText = document.querySelector(".wind-value-text");
+const weatherSummaryImg = document.querySelector(".weather-summery-img");
+const currentDateText = document.querySelector(".current-date-text");
+
 const apiKey = "dec9e32983e1e4b158d7e7b071f4bc9d";
 
 searchBtn.addEventListener("click", () => {
@@ -27,6 +35,26 @@ searchInput.addEventListener("keydown", (e) => {
   }
 });
 
+function getWeatherIcon(id) {
+  if (id <= 232) return "thunderstorm.svg";
+  if (id <= 321) return "drizzle.svg";
+  if (id <= 531) return "rain.svg";
+  if (id <= 622) return "snow.svg";
+  if (id <= 781) return "atmosphere.svg";
+  if (id <= 800) return "clear.svg";
+  else return "clouds.svg";
+}
+
+function getCurrentData() {
+  const currentDate = new Date();
+  const options = {
+    weekday: "short",
+    day: "2-digit",
+    month: "short",
+  };
+  return currentDate.toLocaleDateString("en-GB", options);
+}
+
 async function getFetchData(endpoint, city) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/${endpoint}?q=${city}&appid=${apiKey}&units=metric`;
 
@@ -42,6 +70,23 @@ async function updateWeatherInfo(city) {
     showDisplaySection(notFoundSection);
     return;
   }
+
+  const {
+    name: country,
+    main: { temp, humidity },
+    weather: [{ id, main }],
+    wind: { speed },
+  } = weatherData;
+
+  countryText.innerHTML = country;
+  tempText.innerHTML = `${Math.round(temp)} °C`;
+  conditionText.innerHTML = main;
+  humidityValueText.innerHTML = `${humidity} %`;
+  windValueText.innerHTML = `${speed} M/s`;
+
+  currentDateText.innerHTML = getCurrentData();
+
+  weatherSummaryImg.src = `/weather/${getWeatherIcon(id)}`;
 
   showDisplaySection(WeatherInfoSection);
 }
